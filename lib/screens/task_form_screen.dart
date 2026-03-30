1e13e3e432ac4028b8cfbad1fdca4f240062922c
hunneymandhotra <hunneymandhotra425@gmail.com>
2026-03-30 20:41:57 +0530

    Fix: Remove residual Isar imports and update dependencies

--- lib/screens/task_form_screen.dart
+++ lib/screens/task_form_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
-import 'package:isar/isar.dart' show Id;
 import 'package:lucide_icons/lucide_icons.dart';
 import 'package:uuid/uuid.dart';
 import 'package:google_fonts/google_fonts.dart';
